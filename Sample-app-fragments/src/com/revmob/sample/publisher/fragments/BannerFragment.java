a0ddbb91172b81fd8e0c4a95c80d01cc6f3dc8a4
AnirudhaAgashe <anirudhaagashe@gmail.com>
2014-05-10 23:10:05 +0530

    Added banner to linearlayout of fragment instead of adding it to fragment viewgroup

--- Sample-app-fragments/src/com/revmob/sample/publisher/fragments/BannerFragment.java
+++ Sample-app-fragments/src/com/revmob/sample/publisher/fragments/BannerFragment.java
@@ -9,24 +9,27 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 
 public class BannerFragment extends Fragment {
 	RevMob revmob;
 	RevMobBanner banner;
-	
+
 	@Override
-	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-		View bannerView = inflater.inflate(R.layout.banner_layout, container, false);
-	    return bannerView;
-	 }
-	  
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+		View bannerView = inflater.inflate(R.layout.banner_layout, container,
+				false);
+		return bannerView;
+	}
+
 	public void addBanner(Activity activity) {
 		revmob = RevMob.start(activity);
-	    banner = revmob.createBanner(activity);
-		
-	    View view = (View) getView().findViewById(R.id.bannerBox);
-	    ((ViewGroup) view).addView(banner);
-	  }
-	
-	
-} 
+		banner = revmob.createBanner(activity);
+
+		View view = (View) getView().findViewById(R.id.banner);
+		((ViewGroup) view).removeAllViews();
+		((ViewGroup) view).addView(banner);
+	}
+
+}
